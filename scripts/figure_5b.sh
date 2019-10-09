3b7e6acd6f4d8c0a853d33867fe2d0d1800c7741
Daniel Sakuma <dsakuma@gmail.com>
2019-10-08 23:48:05 -0300

    Remove files not related to denmo

--- scripts/figure_5b.sh
+++
@@ -1,4 +0,0 @@
-python comparisons.py run_denmo cifar10 600 0    150
-python comparisons.py run_denmo cifar10 0   600  150
-python comparisons.py run_denmo cifar10 100 100  150
-python comparisons.py run_denmo cifar10 300 300  150
