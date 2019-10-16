0e2a720c242a84127ea59f01a0088bdc8ae0c6c8
Daniel Sakuma <dsakuma@gmail.com>
2019-10-16 19:23:29 -0300

    Transform in patches

--- src/j_erosion_dataset.py
+++ src/j_erosion_dataset.py
@@ -1,8 +1,8 @@
 from torch.utils.data.dataset import Dataset
-from torchvision import transforms
 from PIL import Image
 from pathlib import Path
-
+from sklearn.feature_extraction import image
+import numpy as np
 class CustomDatasetFromImage(Dataset):
     def __init__(self, img_path, patch_size, transforms=None):
         """
@@ -14,30 +14,36 @@ class CustomDatasetFromImage(Dataset):
         self.img_path = Path(__file__).parent / img_path
         self.patch_size = patch_size
         self.transforms = transforms
-        # self.to_tensor = transforms.ToTensor()
+        img_as_img = Image.open(self.img_path).convert('1')
+        img_as_np = np.array(img_as_img)
+        self.patches = image.extract_patches_2d(img_as_np, (patch_size,patch_size))
+        print(self.patches.shape)
 
 
     def __getitem__(self, index):
         # must return  tensors, numbers, dicts or lists;
-        # data = # Some data read from a file or image
-        img_as_img = Image.open(self.img_path)
-        img_as_tensor = self.transforms(img_as_img)
-        print(img_as_tensor.shape)
+        patch_as_np = self.patches[index]
+        patch_as_tensor = self.transforms(patch_as_np)
+        print(patch_as_tensor.shape)
+        return patch_as_tensor
+        # print(self.patches.shape)
+        # torch.Size([4, 150, 112])
         # return (img, label)
 
     def __len__(self):
-        return count  # of how many patches?
+        return len(self.patches)  # of how many patches?
 
 
 # img, label = CustomDatasetFromImage.__getitem__(None,0)
 
-
+from torchvision import transforms
 transformations = transforms.Compose([transforms.ToTensor()])
 custom_dataset_from_image = \
     CustomDatasetFromImage('../data/J/j.png',
-                            3,
+                            5,
                             transformations)
-custom_dataset_from_image.__getitem__(0)
+print(custom_dataset_from_image.__getitem__(0))
+# print(custom_dataset_from_image.__len__())
 
 # for patches, labels in custom_dataset_from_image:
     # import pdb; pdb.set_trace(
